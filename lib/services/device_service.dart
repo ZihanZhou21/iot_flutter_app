19aadf29824ffc9a91b442e3599d978f6323cb55
zihanzhou21 <zihanzhou1021@gmail.com>
2025-09-08 11:21:51 +1000

    add logger

--- lib/services/device_service.dart
+++ lib/services/device_service.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
 import '../models/device.dart';
 
 class DeviceService {
+  static final logger = Logger();
   static Future<List<Device>> fetchDevices() async {
     final url = Uri.parse('https://iot-flutter.free.beeceptor.com/api/devices');
     final response = await http.get(url);
-
+    logger.i(response.body);
     if (response.statusCode == 200) {
       final List<dynamic> data = jsonDecode(response.body);
       return data.map((e) => Device.fromJson(e)).toList();
